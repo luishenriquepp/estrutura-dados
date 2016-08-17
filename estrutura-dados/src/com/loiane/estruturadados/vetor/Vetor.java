daffc8b95d2eae46da73a991625abe9c7c47cd45
luishenriquepp <peppyno@gmail.com>
2016-08-17 12:17:57 -0300

    Inclusao de uma variavel int privada no vetor para salvar o numero de elementos atual

--- estrutura-dados/src/com/loiane/estruturadados/vetor/Vetor.java
+++ estrutura-dados/src/com/loiane/estruturadados/vetor/Vetor.java
@@ -1,19 +1,31 @@
 package com.loiane.estruturadados.vetor;
 
 public class Vetor {
-    
+        
     private String[] elementos;
+    private int tamanho;
     
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
     }
     
-    public void adiciona(String elemento) {
-        for (int i=0; i<this.elementos.length; i++) {
-            if (this.elementos[i] == null) {
-                this.elementos[i] = elemento;
-                break;
-            }
+//    public void adiciona(String elemento) {
+//        for (int i=0; i<this.elementos.length; i++) {
+//            if (this.elementos[i] == null) {
+//                this.elementos[i] = elemento;
+//                break;
+//            }
+//        }
+//    }
+    
+    public Boolean adiciona(String elemento) {
+        
+        if(this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        } else {
+            return false;
         }
     }
 }
