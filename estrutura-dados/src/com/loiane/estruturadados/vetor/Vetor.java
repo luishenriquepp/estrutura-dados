e588dc8c0ba8f9615a488c3ff7344307f92be1cf
luishenriquepp <peppyno@gmail.com>
2016-08-17 12:49:29 -0300

    Inclusão de metodo na classe vetor que retorna o numero da posicao do elemento, caso ele exista

--- estrutura-dados/src/com/loiane/estruturadados/vetor/Vetor.java
+++ estrutura-dados/src/com/loiane/estruturadados/vetor/Vetor.java
@@ -10,7 +10,7 @@ public class Vetor {
         this.tamanho = 0;
     }
         
-    public Boolean adiciona(String elemento) {
+    public boolean adiciona(String elemento) {
         
         if(this.tamanho < this.elementos.length) {
             this.elementos[this.tamanho] = elemento;
@@ -28,6 +28,15 @@ public class Vetor {
         }
         
         return this.elementos[posicao];
+    }
+    
+    public int busca(String elemento) {
+        for (int i=0; i<this.tamanho; i++) {
+            if(this.elementos[i].equals(elemento)) {
+                return i;
+            }
+        }
+        return -1;
     }
     
     public int tamanho() {
